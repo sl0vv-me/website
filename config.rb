a06fed670e4cf6237f57dd8ffeca167d1e5a753e
Alex Kotov <kotovalexarian@gmail.com>
2019-05-07 23:13:53 +0500

    Add helpers #local_term_names, #global_term_names

--- config.rb
+++ config.rb
@@ -9,3 +9,13 @@ activate :i18n, mount_at_root: :ru
 activate :autoprefixer do |prefix|
   prefix.browsers = 'last 2 versions'
 end
+
+helpers do
+  def global_term_names(term)
+    local_term_names term
+  end
+
+  def local_term_names(term)
+    I18n.translate term, scope: %i[terms names], default: []
+  end
+end
