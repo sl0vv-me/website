3af419d1523fa5f8aa14078ada9509012d587794
Alex Kotov <kotovalexarian@gmail.com>
2019-06-04 03:17:39 +0500

    Move assets into separate directory

--- config.rb
+++ config.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+set :css_dir,    'assets/stylesheets'
+set :fonts_dir,  'assets/fonts'
+set :images_dir, 'assets/images'
+set :js_dir,     'assets/javascripts'
+
 set :sass_assets_paths, %w[node_modules]
 
 activate :i18n, mount_at_root: :ru
