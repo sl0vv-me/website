22aa3d3737019033928d5dac1b380e0c790140ac
Alex Kotov <kotovalexarian@gmail.com>
2020-05-19 10:01:02 +0500

    Add helper #external_link

--- config.rb
+++ config.rb
@@ -2,6 +2,17 @@
 
 set :base_url, 'https://crypto-libertarian.com'
 
+set(
+  :external_links,
+  telegram_channel: 'https://t.me/crypto_libertarian',
+  telegram_chat: 'https://t.me/crypto_libertarian_chat',
+  youtube_channel: 'https://www.youtube.com/channel/UCj9VPPL4riHinL3N9RbmLww',
+  medium_blog: 'https://medium.com/crypto-libertarian',
+  publications_archive: 'https://pub.crypto-libertarian.com',
+  wiki: 'https://wiki.crypto-libertarian.com',
+  git: 'https://git.crypto-libertarian.com',
+)
+
 set :css_dir,    'assets/stylesheets'
 set :fonts_dir,  'assets/fonts'
 set :images_dir, 'assets/images'
@@ -34,6 +45,10 @@ helpers do
 
   def canonical_url
     "#{base_url}#{current_page.url}"
+  end
+
+  def external_link(key)
+    config[:external_links][key] or raise "Invalid key: #{key.inspect}"
   end
 
   def title
